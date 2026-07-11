ff7b6f6910139569f4b5289333c2ec69d63d53c4
Adler Hu <huadlerben1@gmail.com>
2026-07-11 14:02:25 +0000

    update self-introduction

--- apps/features/question_answering.py
+++ apps/features/question_answering.py
@@ -1,2 +1,29 @@
-def question_answering():
-    return '問答功能建置中...'
+from google import genai
+from google.genai import types
+import os
+
+PROJECT_ID = os.environ["PROJECT_ID"]
+
+client = genai.Client(
+    vertexai=True,
+    project=PROJECT_ID,
+    location="global",
+    http_options=types.HttpOptions(api_version="v1"),
+)
+
+
+def question_answering(question: str) -> str:
+
+    prompt = f"""
+請直接回答使用者的問題。
+
+問題：
+{question}
+"""
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=prompt,
+    )
+
+    return response.text
